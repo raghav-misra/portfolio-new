3a7e7fd78f1abba527eb3b3d7323a60e7fb04ada
Raghav <raghav.m2014@gmail.com>
2022-05-24 08:09:53 -0700

    Fix <head> info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,42 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
     css: ["~/styles/index.css"],
     meta: {
-        title: "Raghav Misra"
-    }
+        title: "Raghav Misra",
+        charset: "utf-8",
+        viewport: "width=device-width, initial-scale=1",
+        meta: [
+            {
+                hid: "description",
+                name: "description",
+                content: "High-schooler, full-stack web developer, intern @ Hack+"
+            }
+        ],
+        link: [
+            {
+                rel: "icon",
+                type: "image/x-icon",
+                href: "/favicon.ico"
+            },
+            {
+                rel: "preload",
+                href: "https://fonts.googleapis.com/css2?family=DM+Sans&family=Fira+Code:wght@300&display=swap",
+                as: "style"
+            },
+            {
+                rel: "preload",
+                href: "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.1/css/all.min.css",
+                as: "style"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=DM+Sans&family=Fira+Code:wght@300&display=swap"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.1/css/all.min.css",
+                media: "print",
+                onload: "this.media = 'all';"
+            }
+        ]
+    },
 });
