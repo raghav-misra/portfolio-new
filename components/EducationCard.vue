c3afe1e043a69fafad25f91d790f72c0d8961900
Raghav Misra <raghav.m2014@gmail.com>
2022-08-28 11:55:44 -0700

    add educational

---
+++ components/EducationCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { formatDate } from '~~/composables/formatDate';
+defineProps<{
+    experience: Omit<IExperience, "description">
+}>();
+</script>
+
+<template>
+    <div class="experience-card box">
+        <header>
+            <b class="h4">{{ experience.title }}&nbsp;</b>
+        </header>
+        <a 
+            class="h6"
+            target="_blank"
+            :href="experience.orgSite"    
+        >
+            {{ experience.orgName }}
+        </a>
+        <small style="color: var(--warning);">
+            ({{ formatDate(experience.startDate) }}—{{ formatDate(experience.endDate) }})
+        </small>
+        <p>
+           <slot />
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.experience-card {
+    animation: open-up-x 0.75s;
+}
+</style>
--- pages/experience.vue
+++ pages/experience.vue
@@ -1,25 +1,75 @@
 <script setup lang="ts">
+import Toggle from "@vueform/toggle";
+
+const showProfessionalExperience = ref(true);
+
 useHead({
     title: "Experience"
 });
 
-const { data: experiencesContent } = 
+const { data: experiencesContent } =
     await useAsyncData("experiencesContent", () => queryContent<IExperience>("/experiences").sort({ startDate: -1 }).find());
 
 const currentExperiences = computed(() => experiencesContent.value.filter(e => !e.endDate));
 const pastExperiences = computed(() => experiencesContent.value.filter(e => !!e.endDate));
 </script>
 
+<style src="@vueform/toggle/themes/default.css" />
+
 <template>
     <section class="page">
+
         <header id="top">
             <h1 class="h2">My Experience</h1>
         </header>
 
-        <ExperienceCard v-for="experience of currentExperiences" :experience="experience" />
+        <div class="toggle-row">
+            Educational
+            <Toggle class="experience-toggle" v-model="showProfessionalExperience" />
+            Professional
+        </div>
 
-        <ExperienceCard v-for="experience of pastExperiences" :experience="experience" />
-
-        <BackToTop />
+        <div v-if="showProfessionalExperience">
+            <ExperienceCard v-for="experience of currentExperiences" :experience="experience" />
+            <ExperienceCard v-for="experience of pastExperiences" :experience="experience" />
+            <BackToTop />
+        </div>
+        <div v-else>
+            <EducationCard :experience="{
+                title: 'Applied Data Science Program Student',
+                orgName: 'MIT Professional Education',
+                orgSite: 'https://professional.mit.edu/',
+                startDate: '2022/07'
+            }">
+                
+            </EducationCard>
+            <EducationCard :experience="{
+                title: 'High School Senior',
+                orgName: 'Amador Valley High School',
+                orgSite: 'https://amador.pleasantonusd.net/',
+                startDate: '2019/08'
+            }">
+                <b>Activities</b>: ACE Coding (Instructor), AmadorUAVs (Imaging Lead), Web Development (President)
+            </EducationCard>
+        </div>
     </section>
 </template>
+
+<style scoped>
+.toggle-row {
+    display: flex;
+    align-items: center;
+}
+
+.experience-toggle {
+    margin: 0 0.375rem;
+    --toggle-bg-on: var(--success);
+    --toggle-border-on: var(--success);
+    --toggle-bg-off: var(--warning);
+    --toggle-border-off: var(--warning);
+}
+
+.experience-toggle :deep(.toggle-handle) {
+    box-shadow: 0 0 2px var(--darkest);
+}
+</style>
